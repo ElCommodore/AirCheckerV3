6ae1133fe5c3389107c53ba512c58cba6e419ecf
Alexander Voigt <alexander.voigt@st.ovgu.de>
2018-01-15 16:36:46 +0100

    Ready

--- app/src/main/java/com/aircheckersolutions/airchecker/aircheckerv3/MainActivity.java
+++ app/src/main/java/com/aircheckersolutions/airchecker/aircheckerv3/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aircheckersolutions.airchecker.aircheckerv3;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.StrictMode;
 import android.support.design.widget.TabLayout;
@@ -19,6 +21,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -41,11 +44,23 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.regex.Pattern;
+
+import com.uxcam.UXCam;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,31 +73,92 @@ public class MainActivity extends AppCompatActivity {
     public Pollutant[] pollutants;
     public Pollen[] pollen;
 
+    ProgressDialog progress ;
+
+    public static final String EXTRA_MESSAGE = "com.example.AirCheckerV3.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //UXCam.startWithKey("88d7622c023449b");
+
+        String FILENAME = "config.cfg";
 
 
 
-        pollutants = new Pollutant[6];
-        pollutants[0] = new Pollutant("PM25",0.0f,true, R.drawable.bgpollutantpm25);
-        pollutants[1] = new Pollutant("PM10",0.0f,true, R.drawable.bgpollutantpm10);
-        pollutants[2] = new Pollutant("Ozone",0.0f,true, R.drawable.bgpollutantozone);
-        pollutants[3] = new Pollutant("Nitrogene Dioxide",0.0f,true, R.drawable.bgpollutantno2);
-        pollutants[4] = new Pollutant("Sulfur Dioxide",0.0f,true, R.drawable.bgpollutantso2);
-        pollutants[5] = new Pollutant("Carbone Monooxide",0.0f,true, R.drawable.bgpollutantco);
+        BufferedReader reader;
+        FileInputStream fis = null;
+        String test = "";
+        try{
+            fis = openFileInput(FILENAME);
+            reader = new BufferedReader(new InputStreamReader(fis));
+            StringBuilder sb =  new StringBuilder();
+            while((test = reader.readLine()) != null){
+                sb.append(test);
+            }
+            test = sb.toString();
+            reader.close();
+            fis.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            test = "11111111111111";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final String message = test;
+        List<Pollutant> listPollutant = new ArrayList<Pollutant>();
+        List<Pollen> listPollen = new ArrayList<Pollen>();
+
 
         pollen = new Pollen[8];
-        pollen[0] = new Pollen("Ambrosia","",true, R.drawable.bgpollenambrosia);
-        pollen[1] = new Pollen("Beifuß","",true, R.drawable.bgpollenbeifuss);
-        pollen[2] = new Pollen("Birke","",true, R.drawable.bgpollenbirke);
-        pollen[3] = new Pollen("Erle","",true, R.drawable.bgpollenerle);
-        pollen[4] = new Pollen("Esche","",true, R.drawable.bgpollenesche);
-        pollen[5] = new Pollen("Gräser","",true, R.drawable.bgpollengrass);
-        pollen[6] = new Pollen("Haselnuss","",true, R.drawable.bgpollenhaselnuss);
-        pollen[7] = new Pollen("Roggen","",true, R.drawable.bgpollenrye);
+
+        if(test.charAt(0) == '1')
+            listPollutant.add(new Pollutant("PM25",0.0f,true, R.drawable.bgpollutantpm25));
+        if(test.charAt(1) == '1')
+            listPollutant.add(new Pollutant("PM10",0.0f,true, R.drawable.bgpollutantpm10));
+        if(test.charAt(2) == '1')
+            listPollutant.add(new Pollutant("Ozone",0.0f,true, R.drawable.bgpollutantozone));
+        if(test.charAt(3) == '1')
+            listPollutant.add(new Pollutant("Nitrogene Dioxide",0.0f,true, R.drawable.bgpollutantno2));
+        if(test.charAt(4) == '1')
+            listPollutant.add(new Pollutant("Sulfur Dioxide",0.0f,true, R.drawable.bgpollutantso2));
+        if(test.charAt(5) == '1')
+            listPollutant.add(new Pollutant("Carbone Monooxide",0.0f,true, R.drawable.bgpollutantco));
+
+
+        pollutants = new Pollutant[listPollutant.size()];
+        pollutants = listPollutant.toArray(pollutants);
+
+
+        if(test.charAt(6) == '1')
+            listPollen.add(new Pollen("Ambrosia","",true, R.drawable.bgpollenambrosia));
+        if(test.charAt(7) == '1')
+            listPollen.add(new Pollen("Beifuß","",true, R.drawable.bgpollenbeifuss));
+        if(test.charAt(8) == '1')
+            listPollen.add(new Pollen("Birke","",true, R.drawable.bgpollenbirke));
+        if(test.charAt(9) == '1')
+            listPollen.add(new Pollen("Erle","",true, R.drawable.bgpollenerle));
+        if(test.charAt(10) == '1')
+            listPollen.add(new Pollen("Esche","",true, R.drawable.bgpollenesche));
+        if(test.charAt(11) == '1')
+            listPollen.add(new Pollen("Gräser","",true, R.drawable.bgpollengrass));
+        if(test.charAt(12) == '1')
+            listPollen.add(new Pollen("Haselnuss","",true, R.drawable.bgpollenhaselnuss));
+        if(test.charAt(13) == '1')
+            listPollen.add(new Pollen("Roggen","",true, R.drawable.bgpollenrye));
+
+        pollen = new Pollen[listPollen.size()];
+        pollen = listPollen.toArray(pollen);
+
+        progress = new ProgressDialog(this);
+        progress.setTitle("Loading");
+        progress.setMessage("Wait while loading...");
+        progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+
+
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -92,60 +168,92 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        ImageButton fab = (ImageButton) findViewById(R.id.btn_settings);
+        Button btnSettings = (Button) findViewById(R.id.btn_settings1);
+        final Button fab = (Button) findViewById(R.id.btn_settings);
+
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String httpCode = GetWebsite("https://api.waqi.info/feed/here/?token=16be779fb1a58ee9de6a3f1848a345ada4f4bfc6");
-                try {
-                    JSONObject obj = new JSONObject(httpCode);
-                    pollutants[0].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("pm25").getString("v"));
-                    pollutants[1].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("pm10").getString("v"));
-                    pollutants[2].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("o3").getString("v"));
-                    pollutants[3].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("no2").getString("v"));
-                    pollutants[4].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("so2").getString("v"));
-                    pollutants[5].currentValue = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("co").getString("v"));
+                progress.show();
 
-                    for(int i = 0; i < 6; i++){
-                        pollutants[i].percentValue = pollutants[i].currentValue / pollutants[i].max;
-                        if(pollutants[i].percentValue < 0.2f){
-                            pollutants[i].status = 1;
-                        }
-                        if(pollutants[i].percentValue < 0.5f && pollutants[i].percentValue >= 0.2f){
-                            pollutants[i].status  = 2;
-                        }
-                        if(pollutants[i].percentValue < 0.8f && pollutants[i].percentValue >= 0.5f){
-                            pollutants[i].status  = 3;
-                        }
-                        if(pollutants[i].percentValue <= 1.0f && pollutants[i].percentValue >= 0.8f){
-                            pollutants[i].status  = 4;
-                        }
-                    }
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    System.out.println(e.getMessage());
-                }
-
-                GetWebsiteJS();
+                Refresh();
 
                 mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
                 mViewPager = (ViewPager) findViewById(R.id.container);
                 mViewPager.setAdapter(mSectionsPagerAdapter);
                 mSectionsPagerAdapter.GetData();
-
+                Date currentTime = Calendar.getInstance().getTime();
+                fab.setText("Last Refresh: " + currentTime);
+                progress.dismiss();
             }
         });
 
+        btnSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+
+
+                //finish();
+                //startActivity(getIntent());
+            }
+        });
+
+
+
+    }
+
+    public void Refresh(){
+        String httpCode = GetWebsite("https://api.waqi.info/feed/here/?token=16be779fb1a58ee9de6a3f1848a345ada4f4bfc6");
+        try {
+            JSONObject obj = new JSONObject(httpCode);
+            Float[] pollutantArray = new Float[6];
+
+            pollutantArray[0] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("pm25").getString("v"));
+            pollutantArray[1] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("pm10").getString("v"));
+            pollutantArray[2] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("o3").getString("v"));
+            pollutantArray[3] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("no2").getString("v"));
+            pollutantArray[4] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("so2").getString("v"));
+            pollutantArray[5] = Float.parseFloat(obj.getJSONObject("data").getJSONObject("iaqi").getJSONObject("co").getString("v"));
+
+            for(int i = 0; i < pollutants.length; i++){
+                pollutants[i].currentValue = pollutantArray[i];
+                pollutants[i].percentValue = pollutants[i].currentValue / pollutants[i].max;
+                if(pollutants[i].percentValue < 0.2f){
+                    pollutants[i].status = 1;
+                }
+                if(pollutants[i].percentValue < 0.5f && pollutants[i].percentValue >= 0.2f){
+                    pollutants[i].status  = 2;
+                }
+                if(pollutants[i].percentValue < 0.8f && pollutants[i].percentValue >= 0.5f){
+                    pollutants[i].status  = 3;
+                }
+                if(pollutants[i].percentValue <= 1.0f && pollutants[i].percentValue >= 0.8f){
+                    pollutants[i].status  = 4;
+                }
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+        GetWebsiteJS();
     }
 
     public Pollutant[] GetPollutants(){
@@ -175,7 +283,7 @@ public class MainActivity extends AppCompatActivity {
             builder.append("Error : ").append(e.getMessage()).append("\n");
         }
 
-        for(int i = 0; i<8;i++){
+        for(int i = 0; i<pollen.length;i++){
             pollen[i].currentValue = pollenValues[i];
             if(pollenValues[i].equals("Keine Belastung"));
                 pollen[i].status = 0;
